5f623dc2efe99093101069659f364a6caf37f87e
adhyyanT <adhyyan321@gmail.com>
2024-05-05 23:15:21 +0530

    untrack appSettings

--- backend/MedifMed/obj/Debug/net8.0/MedifMed.AssemblyInfo.cs
+++ backend/MedifMed/obj/Debug/net8.0/MedifMed.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MedifMed")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9ca7a593374db8761c5ae2732eafecd7ab1bd1f2")]
 [assembly: System.Reflection.AssemblyProductAttribute("MedifMed")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MedifMed")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
